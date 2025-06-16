852128db9ee03277d9ec2dfbf9bb9af770d0ea2a
Alberto <albmartinmateos@gmail.com>
2025-06-16 11:38:19 +0200

    lazy loading para no cargar un componente si no es necesario

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
+import { lazy, Suspense, useState } from 'react';
 import './App.css'
 //import ProblematicExample from './components/ProblematicExample/ProblematicExample'
 //import PromiseErrorExample from './components/PromiseErrorExample/PromiseErrorExample'
-import UndefinedExample from './components/UndefinedExample/UndefinedExample'
+//import UndefinedExample from './components/UndefinedExample/UndefinedExample'
 // import CustomModal from './components/BasicExamplePortal/BasicExamplePortal'
 // import TooltipExample from './components/TooltipExample/TooltipExample'
 import ErrorBoundary from './ErrorBoundary'
 // import { Providers } from './providers/Providers'
 // import { AppRouter } from './App.router'
 
+const UndefinedExample = lazy(() => import('./components/UndefinedExample/UndefinedExample.tsx'));
+const ProblematicExample = lazy(() => import('./components/ProblematicExample/ProblematicExample.tsx'));
+const PromiseErrorExample = lazy(() => import('./components/PromiseErrorExample/PromiseErrorExample.tsx'));
+
 function App() {
+  const [selection, setSelection] = useState(0)
+
   return (
     <>
       <ErrorBoundary>
-          <UndefinedExample />
+        <Suspense fallback={<div>Cargando...</div>}>
+        <h2>Todo funciona bien</h2>
+          <button onClick={()=> setSelection(1)}>Selecciona UndefinedExample</button>
+          <button onClick={()=> setSelection(2)}>Selecciona PromiseErrorExample </button>
+          <button onClick={()=> setSelection(3)}>Selecciona ProblematicExample</button>
+          {selection === 1 && <UndefinedExample />}
+          {selection === 2 && <PromiseErrorExample /> }
+          {selection === 3 && <ProblematicExample/> }
+          </Suspense>
       </ErrorBoundary>
     </>
-        //<PromiseErrorExample />
-        //<ProblematicExample/>
-        
+            
     // <CustomModal>hola soy la app </CustomModal>
       // <TooltipExample />
     //  <Providers>
--- src/components/UndefinedExample/UndefinedExample.tsx
+++ src/components/UndefinedExample/UndefinedExample.tsx
@@ -1,7 +1,9 @@
 
 const UndefinedExample = () => {
-    const obj: { prop?: string } = {}; // Propiedad opcional
-    return <div>{obj.prop!.length}</div>; // Usando el operador de aserción no nulo
+    //const obj: { prop?: string } = {}; // Propiedad opcional
+   //return <div>{obj.prop!.length}</div>; // Usando el operador de aserción no nulo
+    return <div>UndefinedExample</div>
 }
+
 
 export default UndefinedExample;
